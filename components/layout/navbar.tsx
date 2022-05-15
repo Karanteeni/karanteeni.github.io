d5fd00b5807e7bd84843dac5b192bfd9c0cba4eb
Matti Turpeinen <matti.mikael.turpeinen@gmail.com>
2022-05-15 18:44:39 +0300

    Hide history from navbar

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -172,7 +172,7 @@ const Navbar = () => {
                 <DropDownItem close={closeNav} icon={faHammer} url="/reseptit" title="Custom reseptit" />
                 <DropDownItem close={closeNav} icon={faQuestionCircle} url="/ukk" title="UKK" />
                 <DropDownItem close={closeNav} icon={faEnvelope} url="/julkaisut" title="Julkaisut" />
-                <DropDownItem close={closeNav} icon={faBookOpen} url="/historia" title="Historia" />
+                {/*<DropDownItem close={closeNav} icon={faBookOpen} url="/historia" title="Historia" />*/}
             </DropDownCollapseMenu>
 
             <NavLink close={closeNav} icon={faExclamation} url="/saannot" title="Säännöt" />
