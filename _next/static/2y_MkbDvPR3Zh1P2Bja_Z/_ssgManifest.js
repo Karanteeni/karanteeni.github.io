a23f2483c400f4b140126eacd56febe1a0edba00
UninenVille <135575164+UninenVille@users.noreply.github.com>
2024-11-30 20:46:15 +0000

    Deploying to gh-pages from  @ 150bf57f53b5d93248e66f3e310a0ba21f3d2e2b 🚀

--- _next/static/2y_MkbDvPR3Zh1P2Bja_Z/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fkauppa","\u002F","\u002Fkauppa\u002Ftuotteet","\u002Fjulkaisut","\u002Fjulkaisut\u002F[id]","\u002Fkauppa\u002Ftuotteet\u002F[type]","\u002Fkauppa\u002Ftuotteet\u002F[type]\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
