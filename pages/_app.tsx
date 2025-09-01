72bbc3c092702e7231e54bd2a733ab564e4e80ce
BuildTools <aarnio.jokune@gmail.com>
2025-09-01 20:48:08 +0300

    Its finally autism!

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,8 +5,8 @@ import { ToastContainer } from 'react-toastify';
 import '../styles/global.css'
 // import '../styles/winter.css'
 // import '../styles/easter.css'
-import '../styles/summer.css'
-// import '../styles/autumn.css'
+// import '../styles/summer.css'
+import '../styles/autumn.css'
 
 function App({ Component, pageProps }: AppProps) {
     return (
